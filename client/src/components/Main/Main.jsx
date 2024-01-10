b8344228132b56b145c0486f8c461bafd1880104
AxelothLeohryn <alejandro97ss@gmail.com>
2024-01-10 16:15:04 +0100

    start/front structure

---
+++ client/src/components/Main/Main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+
+const Main = () => {
+  return <main>Main</main>; 
+  //crear rutas
+};
+
+export default Main;

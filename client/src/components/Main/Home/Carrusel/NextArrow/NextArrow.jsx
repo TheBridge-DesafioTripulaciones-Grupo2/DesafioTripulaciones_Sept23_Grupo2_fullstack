db4c084d1d20075e0ff8f5eb3013b76a635930a3
AxelothLeohryn <alejandro97ss@gmail.com>
2024-01-16 18:51:21 +0100

    add/home achievements, cards, slider add/clientlist sort

---
+++ client/src/components/Main/Home/Carrusel/NextArrow/NextArrow.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NextArrow = () => {
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "grey" }} // Style as needed
+      onClick={onClick}
+    />
+  );
+};
+
+export default NextArrow;

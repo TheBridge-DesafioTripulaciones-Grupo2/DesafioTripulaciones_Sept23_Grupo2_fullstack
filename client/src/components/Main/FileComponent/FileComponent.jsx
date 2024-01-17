9a8ddfc3d4f52d66bb2be8cc61b6e93fc67e9cb8
AxelothLeohryn <alejandro97ss@gmail.com>
2024-01-17 18:12:47 +0100

    add/smooth scroll on bill submit

--- client/src/components/Main/FileComponent/FileComponent.jsx
+++ client/src/components/Main/FileComponent/FileComponent.jsx
@@ -1,18 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import TableComponent from "./TableComponent/";
 import ProposalComponent from "./ProposalComponent";
 
 const FileComponent = () => {
   //Track if the bill is submitted or not (show the proposals section or not)
   const [billSubmitted, setBillSubmitted] = useState(false);
+  const proposalRef = useRef(null);
   // Function to be called by the child component to update the state
   const handleBillSubmit = (isSubmitted) => {
     setBillSubmitted(isSubmitted);
   };
+
+  useEffect(() => {
+    if (billSubmitted && proposalRef.current) {
+      proposalRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [billSubmitted]);
+
   return (
     <section>
       <TableComponent onBillSubmit={handleBillSubmit} />
-      {billSubmitted ? <ProposalComponent /> : null}
+      {billSubmitted ? (
+        <div ref={proposalRef}>
+          <ProposalComponent />
+        </div>
+      ) : null}
       {/* <ProposalComponent /> */}
     </section>
   );
